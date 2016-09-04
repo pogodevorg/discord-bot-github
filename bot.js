54b16285a68b74fdbf8a475d3efbf5c7837fffd5
Lisiano <Lisiano256@users.noreply.github.com>
2016-09-04 18:10:34 +0300

    Logger, gitignore auth.js Add logger to bot.js Move example auth.js to auth.js.example and gitignore auth.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 var forky = require('forky');
 var path = require('path');
 var config = require('./config/auth.js');
+var logger = require('./lib/workers/logger.js');
 
 forky({
     path: path.join(__dirname, '/lib/workers/event.js'),
