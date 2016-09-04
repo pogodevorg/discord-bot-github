23a22d13b19ab64e6b61091448763e1c811e3722
Lisiano <Lisiano256@users.noreply.github.com>
2016-09-04 14:17:46 +0300

    ESLint Switch a couple rules to warnings and ignore newlines and comments for max-lines. Beautify some stuff ESLinting

--- bot.js
+++ bot.js
@@ -2,4 +2,7 @@ var forky = require('forky');
 var path = require('path');
 var config = require('./config/auth.js');
 
-forky({path: path.join(__dirname, '/lib/workers/event.js'), workers: config.web.workers});
+forky({
+    path: path.join(__dirname, '/lib/workers/event.js'),
+    workers: config.web.workers
+});
