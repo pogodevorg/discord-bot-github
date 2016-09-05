76d65d6a4f7dd2688c993d99bcff162cf868204c
Lisiano <Lisiano256@users.noreply.github.com>
2016-09-05 16:39:29 +0300

    Goo.gl Url Shortening (Working) URL shortening will try to shorten the URL, if it fails for any reason, it will send the long url. General code Styling.

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 var forky = require('forky');
 var path = require('path');
 var config = require('./config/auth.js');
-var logger = require('./lib/workers/logger.js');
 
 forky({
     path: path.join(__dirname, '/lib/workers/event.js'),
