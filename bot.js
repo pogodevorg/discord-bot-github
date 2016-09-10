61e403f8357cef84aa693aa1fbab3ed6d170811a
Lisiano <Lisiano256@users.noreply.github.com>
2016-09-10 15:10:23 +0300

    User-error-proofing. The bot should exit if auth.js is missing or if an important config value is missing. The bot will subsitute a default value for any missing config value that can be defaulted. Slight Readme update.

--- bot.js
+++ bot.js
@@ -1,15 +1,37 @@
+/* eslint no-process-exit: [0] */
+
 var forky = require('forky');
 var path = require('path');
 var config = require('./config/auth.js');
 var pkg = require('./package');
 var semver = require('semver');
 
-if (!semver.satisfies(process.version, pkg.engines.node)) {
-    console.log('Requires a node version matching ' + pkg.engines.node);
+function misconfigured(err) {
+    console.log(err);
     process.exit();
+}
+
+if (!semver.satisfies(process.version, pkg.engines.node)) {
+    misconfigured('Requires a node version matching ' + pkg.engines.node + '\nCurrent is ' + process.version);
+}
+
+if (typeof config === 'undefined' || typeof config.github === 'undefined' || typeof config.discord === 'undefined' || typeof config.web === 'undefined') {
+    misconfigured('Missing or horribly misconfigured auth.js');
+}
+
+if (typeof config.github.token === 'undefined') {
+    misconfigured('Requires a github secret token in auth.js');
+}
+
+if (typeof config.discord.token === 'undefined') {
+    misconfigured('Requires a Discord bot token in auth.js');
+}
+
+if (typeof config.discord.channelID === 'undefined') {
+    misconfigured('Requires a Discord Channel ID in auth.js');
 }
 
 forky({
     path: path.join(__dirname, '/lib/workers/event.js'),
-    workers: config.web.workers
+    workers: config.web.workers || 1
 });
