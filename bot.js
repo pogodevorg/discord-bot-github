9af35bc89687d53505a7cbb2c61e613a3a8e1e62
fkndean <admin@darkmatter.zone>
2016-09-10 05:23:53 -0400

    Add node version check. :boom:

--- bot.js
+++ bot.js
@@ -1,6 +1,13 @@
 var forky = require('forky');
 var path = require('path');
 var config = require('./config/auth.js');
+var pkg = require('./package');
+var semver = require('semver');
+
+if(!semver.satisfies(process.version, pkg.engines.node)) {
+  console.log('Requires a node version matching ' + pkg.engines.node);
+  process.exit();
+}
 
 forky({
     path: path.join(__dirname, '/lib/workers/event.js'),
