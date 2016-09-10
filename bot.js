07237976134b3e280e406a1e084c36af726002d0
Lisiano <Lisiano256@users.noreply.github.com>
2016-09-10 12:30:35 +0300

    :potato:

--- bot.js
+++ bot.js
@@ -4,9 +4,9 @@ var config = require('./config/auth.js');
 var pkg = require('./package');
 var semver = require('semver');
 
-if(!semver.satisfies(process.version, pkg.engines.node)) {
-  console.log('Requires a node version matching ' + pkg.engines.node);
-  process.exit();
+if (!semver.satisfies(process.version, pkg.engines.node)) {
+    console.log('Requires a node version matching ' + pkg.engines.node);
+    process.exit();
 }
 
 forky({
